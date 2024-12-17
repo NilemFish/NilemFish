1b6ac1b5139a7c7fb0c618a0a5998dc5e877c0b1
NilemFish <pencariword@gmail.com>
2024-12-17 17:44:03 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,11 +3,5 @@ const navLinks = document.querySelector(".nav-links");
 
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
-});
-const mobileMenu = document.getElementById('mobile-menu');
-const navLinks = document.querySelector('.nav-links');
-
-mobileMenu.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-  mobileMenu.classList.toggle('active');
+  menuToggle.classList.toggle("active"); // Menambahkan animasi pada hamburger
 });
