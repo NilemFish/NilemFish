ebe83d9696bdd24f881f718fdec01b93d0383525
NilemFish <pencariword@gmail.com>
2024-12-17 16:53:42 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
-// Toggle menu saat tombol garis tiga diklik
-const mobileMenu = document.querySelector("#mobile-menu");
-const navLinks = document.querySelector("#nav-links");
+const menuToggle = document.querySelector("#mobile-menu");
+const navLinks = document.querySelector(".nav-links");
 
-mobileMenu.addEventListener("click", () => {
+menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
