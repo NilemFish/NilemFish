215b1444e88e4c25253bdbfa73e83e68b40e35b6
NilemFish <pencariword@gmail.com>
2024-12-17 16:10:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const menuToggle = document.querySelector("#mobile-menu");
-const navList = document.querySelector(".nav-list");
+const navList = document.querySelector(".nav-links");
 
 menuToggle.addEventListener("click", () => {
     navList.classList.toggle("active");
