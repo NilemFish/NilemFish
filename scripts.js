3f4337c31ce681486a2646032577ae22c973d58b
NilemFish <pencariword@gmail.com>
2024-12-17 17:41:49 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,3 +4,10 @@ const navLinks = document.querySelector(".nav-links");
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenu.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  mobileMenu.classList.toggle('active');
+});
