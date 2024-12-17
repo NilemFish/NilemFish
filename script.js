f65704481286fbf04b74780f8eec6d1bec9c5bad
NilemFish <pencariword@gmail.com>
2024-12-17 16:23:12 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const menuToggle = document.querySelector("#mobile-menu");
-  const navList = document.querySelector(".nav-links");
-
-  menuToggle.addEventListener("click", () => {
-    navList.classList.toggle("active");
-  });
-});
