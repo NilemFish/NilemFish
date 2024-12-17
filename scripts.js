7706f21a5015c6315d4b5ef347155597912d9f67
NilemFish <pencariword@gmail.com>
2024-12-17 16:22:37 +0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const menuToggle = document.querySelector("#mobile-menu");
+  const navList = document.querySelector(".nav-links");
+
+  menuToggle.addEventListener("click", () => {
+    navList.classList.toggle("active");
+  });
+});
